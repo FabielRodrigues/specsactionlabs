2fb4158e7d1e4470bbffee383ae6254b4a4c2e46
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2019-05-08 09:04:38 -0300

    Listar tempo parcialmente pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SpecsComponent} from './specs/specs.component';
+import {OpenweatherService} from './services/openweather.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SpecsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, AppRoutingModule, HttpClientModule
   ],
-  providers: [],
+  providers: [OpenweatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
